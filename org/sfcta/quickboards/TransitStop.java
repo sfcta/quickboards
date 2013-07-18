801259314f29b3947322522fb7277d65683beb9e
Elizabeth Sall <elizabeth@sfcta.org>
2013-07-18 13:35:29 -0700

    Add license files

--- org/sfcta/quickboards/TransitStop.java
+++ org/sfcta/quickboards/TransitStop.java
@@ -1,9 +1,32 @@
 /*
- * Created on Apr 19, 2004
+ * Created on Apr 15, 2004 by Billy Charlton
  *
- * (c) 2004 San Francisco County Transportation Authority.
+ * (c) 2004,2010, 2013 San Francisco County Transportation Authority.
  * 
+ *http://github.com/sfcta/quickboards
+ *
+ *Timesheet, Copyright 2013 San Francisco County Transportation Authority
+ *                          San Francisco, CA, USA
+ *                          http://www.sfcta.org/
+ *                          info@sfcta.org
+ *
+ *This file is part of Quickboards.
+ *
+ *Quickboards is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *Quickboards is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with Timesheet.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 package org.sfcta.quickboards;
 
 import java.util.*;
